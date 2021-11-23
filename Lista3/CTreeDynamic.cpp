d21caf7a37db9c90468375b0b3f486d449685115
Kamil <herbetko.kamil@gmail.com>
2021-11-23 12:05:46 +0100

    Adding the rest of the methods.

--- Lista3/CTreeDynamic.cpp
+++ Lista3/CTreeDynamic.cpp
@@ -1,15 +1,15 @@
 #include "CTreeDynamic.h"
 
 CTreeDynamic::CTreeDynamic() {
-
+	pc_root = NULL;
 }
 
 CTreeDynamic::~CTreeDynamic() {
-
+	delete pc_root;
 }
 
 void CTreeDynamic::vPrintTree() {
-
+	pc_root->vPrintAllBelow();
 }
 
 CNodeDynamic::~CNodeDynamic() {
@@ -20,12 +20,24 @@ CNodeDynamic::~CNodeDynamic() {
 
 void CNodeDynamic::vAddNewChild() {
 	CNodeDynamic* pcNodeDynamic = new CNodeDynamic();
+	pcNodeDynamic->pc_parent_node = this;
+	v_children.push_back(pcNodeDynamic);
 }
 
 CTreeDynamic* CNodeDynamic::pcGetChild(int iChildOffset) {
-	return nullptr;
+	if ((0 <= iChildOffset) && (iChildOffset < v_children.size())) {
+		CTreeDynamic* pcTreeDynamic = new CTreeDynamic();
+		CNodeDynamic* pcRoot = pcTreeDynamic->pcGetRoot();
+		pcRoot = v_children.at(iChildOffset);
+		return pcTreeDynamic;
+	}
+
+	return NULL;
 }
 
 void CNodeDynamic::vPrintAllBelow() {
-
+	vPrint();
+	for (int ii = 0; ii < v_children.size(); ii++) {
+		v_children.at(ii)->vPrintAllBelow();
+	}
 }
