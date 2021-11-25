5ad7994df6aa8435cca7862b21865f3da951c699
Kamil <herbetko.kamil@gmail.com>
2021-11-25 15:16:30 +0100

    Adding live modification and adding new atrributes to the node class. Now it stores depth of the tree and on which level given node is.

--- Lista3/CTreeDynamic.cpp
+++ Lista3/CTreeDynamic.cpp
@@ -10,7 +10,7 @@ CTreeDynamic::~CTreeDynamic() {
 
 bool CTreeDynamic::bMoveSubtree(CNodeDynamic* pcParentNode, CNodeDynamic* pcNewChildNode)
 {
-	if ((pcParentNode != NULL) && (pcNewChildNode != NULL)) {
+	if ((pcParentNode != NULL) && (pcNewChildNode != NULL) && (!bNodesAreInTheSameTree(pcParentNode, pcNewChildNode))) {
 		pcNewChildNode->vMoveHelper(pcParentNode);
 		return true;
 	}
@@ -18,9 +18,26 @@ bool CTreeDynamic::bMoveSubtree(CNodeDynamic* pcParentNode, CNodeDynamic* pcNewC
 	return false;
 }
 
+bool CTreeDynamic::bNodesAreInTheSameTree(CNodeDynamic* pcNode1, CNodeDynamic* pcNode2) {
+	CNodeDynamic* pcRoot1 = pcNode1;
+	CNodeDynamic* pcRoot2 = pcNode2;
+
+	while (pcRoot1->pcGetParent() != NULL) {
+		pcRoot1 = pcRoot1->pcGetParent();
+	}
+
+	while (pcRoot2->pcGetParent() != NULL) {
+		pcRoot2 = pcRoot2->pcGetParent();
+	}
+
+	return pcRoot1 == pcRoot2;
+
+}
+
 void CTreeDynamic::vSetRoot(CNodeDynamic* pcNewRootNode) {
 	pc_root = pcNewRootNode;
 }
+
 
 void CTreeDynamic::vPrintTree() {
 	pc_root->vPrintAllBelow();
@@ -35,6 +52,20 @@ CNodeDynamic::~CNodeDynamic() {
 void CNodeDynamic::vAddNewChild() {
 	CNodeDynamic* pcNodeDynamic = new CNodeDynamic();
 	pcNodeDynamic->pc_parent_node = this;
+	pcNodeDynamic->i_level = i_level + 1;
+
+
+	CNodeDynamic* pcRoot = this;
+	
+	while (pcRoot->pc_parent_node != NULL) {
+		pcRoot = pcRoot->pc_parent_node;
+	}
+
+	if (pcRoot->i_depth < i_level + 1) {
+		pcRoot->i_depth = i_level + 1;
+	}
+
+	pcNodeDynamic->i_depth = i_depth;
 	v_children.push_back(pcNodeDynamic);
 }
 
@@ -48,6 +79,39 @@ CTreeDynamic* CNodeDynamic::pcGetChild(int iChildOffset) {
 	return NULL;
 }
 
+void CNodeDynamic::vUpdateDepthAndLevel() {
+	CNodeDynamic* pcRoot = this;
+
+	while (pcRoot->pc_parent_node != NULL) {
+		pcRoot->pc_parent_node;
+	}
+	pcRoot->i_level = 0;
+	pcRoot->vUpdateLevel();
+	pcRoot->vUpdateDepth();
+
+}
+
+void CNodeDynamic::vUpdateDepth() {
+	i_depth = i_level;
+	for (int ii = 0; ii < v_children.size(); ii++) {
+		v_children.at(ii)-> vUpdateDepth();
+	}
+	
+	for (int ii = 0; ii < v_children.size(); ii++)	{
+		if (v_children.at(ii)->i_level > i_depth) {
+			i_depth = v_children.at(ii)->i_level;
+		}
+	}
+}
+
+void CNodeDynamic::vUpdateLevel() {
+	for (int ii = 0; ii < v_children.size(); ii++)
+	{
+		v_children.at(ii)->i_level = i_level + 1;
+		v_children.at(ii)->vUpdateLevel();
+	}
+}
+
 void CNodeDynamic::vMoveHelper(CNodeDynamic* pcNewParent) {
 	(pcNewParent->v_children).push_back(this);
 	if (pc_parent_node != NULL) {
@@ -58,10 +122,33 @@ void CNodeDynamic::vMoveHelper(CNodeDynamic* pcNewParent) {
 					vCurrentParentChildren->erase(vCurrentParentChildren->begin() + ii);
 				}
 			}
+
+			vCurrentParentChildren->back()->vUpdateDepthAndLevel();
 	}
+	
+	pcNewParent->iGetChildrenNumber();
 
 	pc_parent_node = pcNewParent;
 	
+}
+
+void CNodeDynamic::vPrintBreadthHelper(int iNum) {
+	if (i_level == iNum) {
+		vPrint();
+	}
+	else {
+		for (int ii = 0; ii < v_children.size(); ii++) {
+			v_children.at(ii)->vPrintBreadthHelper(iNum);
+		}
+	}
+}
+
+
+void CNodeDynamic::vPrintBreadth() {
+	for (int ii = 0; ii <= i_depth; ii++) {
+		vPrintBreadthHelper(ii);
+		std::cout << "\n";
+	}
 }
 
 void CNodeDynamic::vPrintAllBelow() {
--- Lista3/CTreeDynamic.h
+++ Lista3/CTreeDynamic.h
@@ -6,21 +6,33 @@ class CTreeDynamic;
 
 class CNodeDynamic {
 public:
-	CNodeDynamic() { i_val = 0; pc_parent_node = NULL; };
+	CNodeDynamic() { i_val = 0; pc_parent_node = NULL; i_level = 0; i_depth = 0; };
 	~CNodeDynamic();
 
 	void vSetValue(int iNewVal) { i_val = iNewVal; };
 	int iGetChildrenNumber() { return(v_children.size()); };
 	void vAddNewChild();
 	CTreeDynamic* pcGetChild(int iChildOffset);
-	void vMoveHelper(CNodeDynamic* pcNewParent);
+	void vUpdateDepthAndLevel();
 
+	void vUpdateLevel();
+
+	void vUpdateDepth();
+
+	void vMoveHelper(CNodeDynamic* pcNewParent);
+	void vPrintBreadthHelper(int iNum);
+	CNodeDynamic* pcGetParent() { return pc_parent_node; };
+
+	
 	void vPrint() { std::cout << " " << i_val; };
+	void vPrintBreadth();
 	void vPrintAllBelow();
 private:
 	std::vector<CNodeDynamic*> v_children;
 	CNodeDynamic* pc_parent_node;
 	int i_val;
+	int i_level;
+	int i_depth;
 };
 
 class CTreeDynamic {
@@ -30,9 +42,11 @@ public:
 
 	CNodeDynamic* pcGetRoot() { return(pc_root); }
 	bool bMoveSubtree(CNodeDynamic* pcParentNode, CNodeDynamic* pcNewChildNode);
+	bool bNodesAreInTheSameTree(CNodeDynamic* pcNode1, CNodeDynamic* pcNode2);
 
 	void vSetRoot(CNodeDynamic* pcNewRootNode);
 
+	void vPrintBreadth() { pc_root->vPrintBreadth(); };
 	void vPrintTree();
 private:
 	CNodeDynamic* pc_root;
