ae9421cd992c67e5052449ef635149ae68c08086
Kamil_Herbetko <herbetko.kamil@gmail.com>
2021-12-20 13:03:11 +0100

    Adding live modification.

--- Lista5/CMySmartPointer.h
+++ Lista5/CMySmartPointer.h
@@ -1,15 +1,46 @@
 #pragma once
+#include <vector>
+#include <iostream>
 
-class CRefCounter
-{
+template <typename T> class CMySmartPointer;
+
+template <typename T> class CRefCounter {
 public:
-	CRefCounter() { i_count = 0; }
+	CRefCounter() { i_count = 0;}
 	int iAdd() { return(++i_count); }
-	int iDec() { return(--i_count); };
+	int iDec() { return(--i_count); }
 	int iGet() { return(i_count); }
+
+	void vPrintPointers();
+	void vAddPointer(CMySmartPointer<T>* pcNewSmartPointer);
+	void vRemovePointer(CMySmartPointer<T>* pcSmartPointerToRemove);
+	std::vector<CMySmartPointer<T>*>* vecGetSmartPointers() { return &vec_CSmartPointers; }
+
 private:
 	int i_count;
+	std::vector<CMySmartPointer<T>*> vec_CSmartPointers;
 };
+
+template <typename T>
+void CRefCounter<T>::vPrintPointers() {
+	for (int ii = 0; ii < vec_CSmartPointers.size(); ii++) {
+		std::cout << vec_CSmartPointers.at(ii) << std::endl;
+	}
+}
+
+template <typename T>
+void CRefCounter<T>::vAddPointer(CMySmartPointer<T>* pcNewSmartPointer) {
+	vec_CSmartPointers.push_back(pcNewSmartPointer);
+}
+
+template <typename T>
+void CRefCounter<T>::vRemovePointer(CMySmartPointer<T>* pcNewSmartPointer) {
+	for (int ii = 0; ii < vec_CSmartPointers.size(); ii++) {
+		if (vec_CSmartPointers.at(ii) == pcNewSmartPointer) {
+			vec_CSmartPointers.erase(vec_CSmartPointers.begin() + ii);
+		}
+	}
+}
 
 
 template <typename T> class CMySmartPointer {
@@ -18,16 +49,19 @@ public:
 	CMySmartPointer(const CMySmartPointer& pcOther);
 	~CMySmartPointer();
 	
+	CRefCounter<T>* pcGetCounter() { return pc_counter; }
 	void operator=(const const CMySmartPointer<T>& pcOther);
 	T& operator*() { return(*pc_pointer); }
 	T* operator->() { return(pc_pointer); }
 private:
-	CRefCounter* pc_counter;
+	CRefCounter<T>* pc_counter;
 	T* pc_pointer;
 };
 
 template <typename T>
 CMySmartPointer<T>::~CMySmartPointer() {
+	pc_counter->vRemovePointer(this);
+
 	if (pc_counter->iDec() == 0)
 	{
 		delete pc_pointer;
@@ -40,17 +74,21 @@ CMySmartPointer<T>::CMySmartPointer(const CMySmartPointer<T>& pcOther) {
 	pc_pointer = pcOther.pc_pointer;
 	pc_counter = pcOther.pc_counter;
 	pc_counter->iAdd();
+	pc_counter->vAddPointer(this);
 }
 
 template <typename T>
 CMySmartPointer<T>::CMySmartPointer(T* pcPointer) {
 	pc_pointer = pcPointer;
-	pc_counter = new CRefCounter();
+	pc_counter = new CRefCounter<T>();
 	pc_counter->iAdd();
+	pc_counter->vAddPointer(this);
 }
 
 template <typename T>
 void CMySmartPointer<T>::operator=(const CMySmartPointer<T>& pcOther) {
+	pc_counter->vRemovePointer(this);
+
 	if (pc_counter->iDec() == 0)
 	{
 		delete pc_pointer;
@@ -60,4 +98,5 @@ void CMySmartPointer<T>::operator=(const CMySmartPointer<T>& pcOther) {
 	pc_pointer = pcOther.pc_pointer;
 	pc_counter = pcOther.pc_counter;
 	pc_counter->iAdd();
+	pc_counter->vAddPointer(this);
 }
